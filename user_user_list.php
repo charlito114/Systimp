b4761305e3215e60c86d152eb6bddd1404c701e1
dsquieng44 <49422849+dsquieng44@users.noreply.github.com>
2019-12-04 14:33:18 +0800

    removed print

--- user_user_list.php
+++ user_user_list.php
@@ -183,14 +183,6 @@
                             
                         </div>
                         
-                        <!-- Print and Page -->
-                        <div class="d-flex" style=" margin-top: 0;">
-                            <!-- Print Button-->
-                            <div style="width: 80%;">
-                                <button name="print" value="print" formaction="" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" style="margin-left: 30px; width: 100px;"> Print </button>
-                            </div>
-                        </div>
-                        
                         <!-- Table -->
                         <div class="table-responsive">
                             <form method = 'post' action = ''>
