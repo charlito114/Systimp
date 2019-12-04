24bc56d56585aae6b6b2a4a68881f458d30dee9c
dsquieng44 <49422849+dsquieng44@users.noreply.github.com>
2019-12-04 15:22:17 +0800

    Update purchasing_dashboard.php

--- purchasing_dashboard.php
+++ purchasing_dashboard.php
@@ -155,7 +155,7 @@
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Ongoing Supplier Deliveries</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                               <?php
-                                              $OSDcount = "SELECT COUNT(Status) c FROM p_purchasingmanagement WHERE status='ongoing'";
+                                              $OSDcount = "SELECT COUNT(Status) c FROM p_purchasingmanagement WHERE status='pending'";
                                               $count_result = mysqli_query($con, $OSDcount);
                                               $row = $count_result->fetch_assoc();
                                               echo $row['c'];
@@ -236,7 +236,7 @@
                                       <tbody>
                                         <!-- when po num is clicked and viewed and clicks back to go back to dashboard initially, it goes back to history instead -->
                                         <?php
-                                          $viewTop = "SELECT * FROM p_purchasingmanagement WHERE status ='ongoing' ORDER BY PONum ASC LIMIT 5";
+                                          $viewTop = "SELECT * FROM p_purchasingmanagement WHERE status ='pending' ORDER BY PONum ASC LIMIT 5";
                                         $search_result = mysqli_query($con, $viewTop);
                                         if ($search_result->num_rows > 0) {
                                             // output data of each row
