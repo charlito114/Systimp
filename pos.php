5df1a3f6023659999d3a2b3f880f492d8bef594a
dsquieng44 <49422849+dsquieng44@users.noreply.github.com>
2019-12-16 10:22:49 +0800

    update notif

--- pos.php
+++ pos.php
@@ -140,7 +140,7 @@
                           </div>
                         </li>
 
-                                               <!-- COPY START -->
+                                                <!-- COPY START -->
                         <!-- Nav Item - Alerts -->
                         <li class="nav-item dropdown no-arrow mx-1">
                           <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
